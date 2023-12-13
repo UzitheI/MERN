54ccbc72807bd78643836254e15ae42540673999
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-12-13 17:35:07 +0545

    setting up my backend

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+//extracting the express functionalities from the nodemodules
+
+require('dotenv').config()
+//this is a node feature where we can require the dot env to hide our port number
+
+const app = express();
+//express app created and stored to app
+
+
+
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next()
+})
+//this is called a middelware which we want to run before running the response which can also be called as teh middleware
+//basically this is logging the path and the port which can be useful to us later
+
+
+
+
+//now we have to listen to the request
+app.listen(process.env.PORT,()=>{
+    console.log("listening on port",process.env.PORT)
+})
+//listening is where we want to take our requests in
+//rerun the server to change any changes
+//an alternative to keep refreshing your server is to download nodemon
+//use npm install nodemon with global to make the changes(already done)
+
+//with .env
+//we used process.env.PORT to obtain port number that is currently hidden in the .env file
+//we used process.env.PORT to obtain port number that is currently hidden in the .env file
+
+
+
+
+//now to react with requests we create a route
+app.get('/',(req,res)=>{
+    res.json({mssg:'Welcome to the app'});
+})
+//here req is the request that is being sent and res is the response, whenever we get a request from the port 4000 that is siginified by / , we respond it to it by giving a json string which here is Welcome to the app
+
+
+//another important thing is port number is a sensitive information and we cannot disclose it when we are pushing it in repositories, so we can store it in git ignore files to keep it hidden, so we create a .env file
+
+
+
+
