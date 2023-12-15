1589568677ede04fefc1253a1481e4427bae3adb
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-12-15 16:35:37 +0545

    adding additional controllers

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,35 +1,19 @@
 const express =require('express')
 const router=express.Router()
-const Workout=require('../models/workoutModel')
+const{
+    createWorkout,getWorkouts,getWorkout,deleteWorkout,updateWorkout,
+}=require('../controllers/workoutController')
 
 //this is to get all workouts
-router.get('/',(req,res)=>{
-    res.json({mssg:"Get workouts"})
-
-})
+router.get('/',getWorkouts)
 //get a single workout, here : means the id keeps on changing
-router.get('/:id',(req,res)=>{
-    res.json({mssg:'Get single workout'})
-})
+router.get('/:id',getWorkout)
+
 //post a new workout
-router.post('/',async(req,res)=>{
-    const {title,load,reps}=req.body
-    try{
-    const workout=await Workout.create({title,load,reps})
-    res.status(200).json(workout)
-    }
-    catch(error){
-    res.status(400).json({error: error.message})
-    }
-    res.json({mssg:'post a new workout'})
-})
+router.post('/',createWorkout)
 //delete a workout
-router.delete('/:id',(req,res)=>{
-    res.json({mssg:'delete a workout'})
-})
+router.delete('/:id',deleteWorkout)
 //update a workout
-router.patch('/:id',(req,res)=>{
-    res.json({mssg:'patch a new workout'})
-})
+router.patch('/:id',updateWorkout)
 
 module.exports=router
