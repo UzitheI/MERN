a324e866b3d989352e8b9fe6fecd1aaa19a04bfd
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-02-13 18:01:14 +0545

    function correction

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,24 @@
+import {useEffect,useState} from 'react'
+
 export default function Home(){
+    const[workouts,setWorkouts]=useState(null)
+    useEffect(()=>{
+        const fetchWorkouts= async()=>{
+            const response = await fetch('api/workouts')
+        const json= await response.json()
+        if(response.ok){
+            setWorkouts(json)
+        }
+        }
+        fetchWorkouts()
+    }, [])
     return(
         <div className="Home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map(() => (
+                    <p key={workouts._id}>{workouts.title}</p>
+                ))}
+            </div>
 
         </div>
     )
